a2282b7ade203a26097b61145f85f8fc42ba5780
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2023-07-10 23:01:32 -0300

    realizando as operacoes matematicas

--- src/main/java/br/com/gilson/apigateway/MathController.java
+++ src/main/java/br/com/gilson/apigateway/MathController.java
@@ -8,15 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathController {
 
-	@GetMapping("/sum/{num1}/{num2}")
-	public ResponseEntity<Double> sum(@PathVariable(value = "num1") String num1, @PathVariable(value = "num2") String num2)
+	@GetMapping("/operations/{num1}/{num2}/{operator}")
+	public ResponseEntity<Double> operations(@PathVariable(value = "num1") String num1,
+			@PathVariable(value = "num2") String num2, @PathVariable(value = "operator") String operator)
 			throws Exception {
 
 		if (!isNumeric(num1) || !isNumeric(num2)) {
-			System.out.println("entrou"+ num1 + num2);
 			throw new UnsupportedOperationException("Please set a numeric value!");
 		}
-		var result = Double.parseDouble(num1) + Double.parseDouble(num2);
+
+		var result = calculationOfOperations(num1, num2, operator);
+
 		return ResponseEntity.ok(result);
 	}
 
@@ -29,4 +31,27 @@ public class MathController {
 		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 
+	private double calculationOfOperations(String num1, String num2, String operator) {
+
+		switch (operator) {
+		case "+": {
+			return Double.parseDouble(num1) + Double.parseDouble(num2);
+		}
+		case "-": {
+			return Double.parseDouble(num1) - Double.parseDouble(num2);
+		}
+		case "x": {
+			return Double.parseDouble(num1) * Double.parseDouble(num2);
+		}
+		case "÷": {
+			if (Double.parseDouble(num2) == 0) {
+				throw new UnsupportedOperationException("Division by zero is not defined!");
+			}
+			return Double.parseDouble(num1) / Double.parseDouble(num2);
+		}
+		default:
+			throw new UnsupportedOperationException("Unsupported operation! accepted operators['+', '-', 'x', '÷']");
+		}
+
+	}
 }
